2c7f2c68b166c5fd338f7458cd63c01ed1bd7837
wellysonvie <wellysonvie@gmail.com>
2021-07-14 11:31:07 -0300

    feat: footer implementation

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 text-center w-full">
+      <p className="text-gray-400 dark:text-gray-600">
+        by{" "}
+        <a
+          className="hover:underline font-medium"
+          href="https://github.com/wellysonvie"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @wellysonvie
+        </a>{" "}
+        | 2021
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
