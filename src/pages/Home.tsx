d159860acaec636d7e723b1f1684033d7bf48365
wellysonvie <wellysonvie@gmail.com>
2021-07-16 01:50:49 -0300

    feat: adding scroll indicator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,25 +35,52 @@ const Home = () => {
             + Novo Pagamento
           </button>
         </header>
-        <div className="flex-grow overflow-x-auto pr-1">
-          <table className="w-full border-separate">
-            <thead>
-              <tr
-                className="bg-white shadow-sm dark:bg-gray-700 font-bold 
+        <div className="flex-grow overflow-hidden relative">
+          <div className="w-full h-full overflow-x-auto pr-1">
+            <table className="w-full border-separate">
+              <thead>
+                <tr
+                  className="bg-white shadow-sm dark:bg-gray-700 font-bold 
                           text-gray-400 dark:text-gray-400"
-              >
-                <td className="py-3 px-6 rounded-l">Descrição</td>
-                <td className="py-3 px-6">Valor (R$)</td>
-                <td className="py-3 px-6">Vencimento</td>
-                <td className="py-3 px-6 rounded-r"></td>
-              </tr>
-            </thead>
-            <tbody>
-              {payments.map((payment) => (
-                <Payment key={payment.id} data={payment} />
-              ))}
-            </tbody>
-          </table>
+                >
+                  <td className="py-3 px-6 rounded-l">Descrição</td>
+                  <td className="py-3 px-6 whitespace-nowrap">Valor (R$)</td>
+                  <td className="py-3 px-6">Vencimento</td>
+                  <td className="py-3 px-6 rounded-r"></td>
+                </tr>
+              </thead>
+              <tbody>
+                {payments.map((payment) => (
+                  <Payment key={payment.id} data={payment} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div
+            className="block sm:hidden absolute bottom-0 right-0 z-10 w-6 h-full 
+              flex items-center justify-end 
+              bg-gradient-to-r from-transparent to-gray-100 dark:to-gray-800"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-gray-400 dark:text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              />
+            </svg>
+          </div>
+          <div
+            className="absolute bottom-0 left-0 z-10 w-full h-6 
+            bg-gradient-to-b from-transparent to-gray-100 dark:to-gray-800"
+          ></div>
         </div>
       </section>
       <NewPaymentModal
